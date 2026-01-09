6bceeaafe6d0673d88274a4c19567b244103fe12
thisispaniz <paniz.ziaie@gmail.com>
2026-01-09 19:37:18 +0100

    ingredients and call from the backend

--- frontend/rand-a-pizza/src/App.jsx
+++ frontend/rand-a-pizza/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import NavBar from './components/NavBar'
+import Ingredients from './components/Ingredients'
 import './App.css'
 
 function App() {
@@ -9,10 +9,24 @@ function App() {
   return (
     <>
       <div className="landing-header">
+        {/* The Pizza Image (Absolute positioned background layer) */}
         <div className="pizza-header" />
         
-         <NavBar />
+        {/* Navigation */}
+        <NavBar />
+        
+        {/* Text Content (Left Side) */}
+        <div className="landing-content">
+           <h1>Find your new favorite Pizza!</h1>
+           <button className='landing-button'>Get started now!</button>
+        </div>
       </div>
+
+      <div className="discover-section">
+        <h2>Discover new recipes INSTANTLY!</h2>
+        <p className="discover-subtext">Get unique combos based on flavor profiles and your preference using AI</p>
+      </div>
+      <Ingredients />
     </>
   )
 }
---
+++ frontend/rand-a-pizza/src/components/Ingredients.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../App.css";
+import {Link} from "react-router-dom";
+
+function Ingredients() {
+    return (
+        <body>
+            <h1>We have the following ingredients: </h1>
+            <p></p>
+        </body>
+    )
+}
+
+export default Ingredients;
