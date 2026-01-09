3387a49a2f1a37ecc2f807eb162f0320b5cbb06e
Franzi <115472701+xginevra@users.noreply.github.com>
2026-01-09 08:56:38 +0000

    configured vite.config to connect to backend

--- frontend/rand-a-pizza/vite.config.js
+++ frontend/rand-a-pizza/vite.config.js
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0',
+    port: 3001,
+    https: false,
+    strictPort: true,
+    proxy: {
+  '/api/ingredients': {
+    target: 'http://backend:8000',
+    changeOrigin: true,
+    secure: false,
+  },
+  '/api/random-pizza': {
+    target: 'http://backend:8000',
+    changeOrigin: true,
+    secure: false,
+  },
+  '/api/save-recipe': {
+    target: 'http://backend:8000',
+    changeOrigin: true,
+    secure: false,
+  },
+  '/api/user-recipes/{user_id}': {
+    target: 'http://backend:8000',
+    changeOrigin: true,
+    secure: false,
+  }
+  }
+    }
+      }
+        )
