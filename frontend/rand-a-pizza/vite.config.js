9a62e6977f96973467a02ae4ba940ecbfe720fe1
ABiteofPi <payabiteofpi@gmail.com>
2026-01-10 02:39:29 +0100

    frontend and backend connected - ingredient list fetched and displayed for test

--- frontend/rand-a-pizza/vite.config.js
+++ frontend/rand-a-pizza/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+        router: req => {
+          const hostHeader = req.headers?.host?.split(':')[0] || 'localhost';
+          return `http://${hostHeader}:8000`;
+        },
+      },
+    },
+  },
 })
