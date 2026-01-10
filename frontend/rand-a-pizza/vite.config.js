be7a7e07648c2239712db2bc1d75da77f9bfb3b1
Franzi <115472701+xginevra@users.noreply.github.com>
2026-01-10 13:19:25 +0000

    contributed to connecting frontend and backend

--- frontend/rand-a-pizza/vite.config.js
+++ frontend/rand-a-pizza/vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
