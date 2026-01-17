58fafc76603716b33b766933999a0a73700c4ccd
thisispaniz <paniz.ziaie@gmail.com>
2026-01-17 13:12:58 +0100

    Created the page

---
+++ frontend/rand-a-pizza/pages/CreateAPizza.jsx
@@ -0,0 +1,12 @@
+import React from "reaxt";
+import '../App.css';
+
+const CreateAPizza = () => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+export default CreateAPizza;
