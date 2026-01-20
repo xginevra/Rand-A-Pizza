5541d005dc1e011e57f68d01ac801393af8332fd
ABiteofPi <payabiteofpi@gmail.com>
2026-01-21 00:49:13 +0100

    create pizza page updated

--- frontend/rand-a-pizza/src/pages/CreateAPizza.jsx
+++ frontend/rand-a-pizza/src/pages/CreateAPizza.jsx
@@ -4,16 +4,16 @@ import NavBar from "../components/NavBar";
 
 const CreateAPizza = () => {
   return (
-    <div>
+    <>
       <NavBar />
       <div className="page-heading">
         <h1 className="page-title">Pizza assembly station</h1>
         <h4 className="page-title">
-          ... Create your perfect pizza with the available ingredients ...
+          Create your perfect pizza with the available ingredients, or use "Surprise Me!" to get a random pizza generated for you.
         </h4>
       </div>
       <CreatePizza />
-    </div>
+    </>
   );
 };
 
